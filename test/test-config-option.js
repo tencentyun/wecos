46776035368ff3ceae7be49a089398906027f06b
carsonxu <459452372@qq.com>
2017-01-06 21:58:58 +0800

    修复报错

--- test/test-config-option.js
+++ test/test-config-option.js
@@ -1,19 +1,4 @@
 'use strict';
-
+var config = require('./wecos.config.js');
 var wecos = require('../lib/watcher.js');
-wecos({
-    "appDir": "./app",
-    "backupDir": "./wecos_backup",
-    "uploadFileSuffix": [".jpg", ".png", ".gif"],
-    "uploadFileBlackList": [],
-    "compress": false,
-    "watch": false,
-    "cos": {
-        "appid": "",
-        "bucketname": "",
-        "folder": "",
-        "region": "",
-        "secret_key": "",
-        "secret_id": ""
-    }
-});
+wecos(config);
