e5066e925521352b1a48cd34837ab2d0f7e50f27
lizhongyuan3 <lizhongyuan3@yeah.net>
2022-10-22 15:53:24 +0800

    增加同步到七牛云的配置，增加环境配置

--- test/test-upload.js
+++ test/test-upload.js
@@ -1,6 +1,6 @@
 var path = require('path');
 var upload = require('../lib/upload');
 
-upload(path.join(__dirname, 'test.png'), '/test.jpg', function(res) {
-	console.log(res);
+upload(path.join(__dirname, 'test.jpg'), 'test.jpg', async function(res, url) {
+	console.log((await url).Url);
 });
